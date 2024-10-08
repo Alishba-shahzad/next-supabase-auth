2a1602c95fcc323683a53a35d3c8e2bb0b35a127
Naqqash Abbassi <94614236+naqqashatdostai@users.noreply.github.com>
2024-10-08 17:46:52 +0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
@@ -5,40 +6,50 @@ import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 import UploadButton from "@/components/upload";
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from "next/link";
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-  const supabase = createClient();
-  const { data: tradingAccountStatement } = await supabase
-    .from("trading_account_statement")
-    .select();
-  console.log("notes", tradingAccountStatement);
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
-  console.log("isSupabaseConnected", isSupabaseConnected);
+function NavbarHeader() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <div></div>
-          {isSupabaseConnected && <AuthButton />}
-        </div>
-      </nav>
-
+    <>
+      <Navbar expand="lg" className="navbar-dark bg-dark w-100">
+        <Container>
+          <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Link
+              href="/login"
+              className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+            >
+              Login
+            </Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
         {/* write code to implement upload functionality for uploading csv files */}
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 p-8 bg-white rounded">
           <h2 className="text-2xl font-bold">Upload your CSV</h2>
           <p>
             Upload your CSV file to get started. We will automatically create a
@@ -61,6 +72,10 @@ export default async function Index() {
           </a>
         </p>
       </footer>
-    </div>
+
+    </>
+
   );
 }
+
+export default NavbarHeader;
